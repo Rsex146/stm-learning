6576b0954eab38311d61d84d3257f029ca91b260
Kostya Ryabinin <kostya@nulana.com>
2020-11-07 16:58:45 +0500

    software pwm

--- src/main.c
+++ src/main.c
@@ -6,7 +6,16 @@
 void myDelay(uint32_t t)
 {
 	uint32_t i = 0;
+	t *= 7.2;
 	for (i = 0; i < t; ++i){__NOP();};
+}
+
+void PWM(uint32_t tau, uint32_t T)
+{
+	GPIO_SetBits(GPIOE, GPIO_Pin_9);
+	myDelay(tau);
+	GPIO_ResetBits(GPIOE, GPIO_Pin_9);
+	myDelay(T - tau);
 }
 
 int main()
@@ -14,27 +23,16 @@ int main()
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
     GPIO_InitTypeDef g;
 
-    g.GPIO_Pin = 0xFF00;
+    g.GPIO_Pin = GPIO_Pin_9;
     g.GPIO_Mode = GPIO_Mode_OUT;
     g.GPIO_Speed = GPIO_Speed_Level_1;
     g.GPIO_OType = GPIO_OType_PP;
     g.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOE, &g);
 
-    uint16_t v = 0x1;
-
     while (1)
     {
-    	//GPIO_SetBits(GPIOE, v);
-    	GPIOE->ODR = v << 8;
-    	v = (v << 1) % 0x00FF;
-
-    	//GPIO_Set
-    	//GPIOE->ODR = 512;
-    	myDelay(3200000);
-    	//GPIO_ResetBits(GPIOE, 0xFF00);
-    	//GPIOE->ODR = 0;
-    	//myDelay(3200000);
+    	PWM(5, 1000);
     }
 
     return 0;
