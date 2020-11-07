c7f56418bd9e2cb213e9795dea14a5e7bad6ac27
Kostya Ryabinin <kostya@nulana.com>
2020-11-07 16:28:12 +0500

    running light

--- src/main.c
+++ src/main.c
@@ -14,19 +14,27 @@ int main()
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
     GPIO_InitTypeDef g;
 
-    g.GPIO_Pin = GPIO_Pin_9;
+    g.GPIO_Pin = 0xFF00;
     g.GPIO_Mode = GPIO_Mode_OUT;
     g.GPIO_Speed = GPIO_Speed_Level_1;
     g.GPIO_OType = GPIO_OType_PP;
     g.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOE, &g);
 
+    uint16_t v = 0x1;
+
     while (1)
     {
-    	GPIO_SetBits(GPIOE, GPIO_Pin_9);
+    	//GPIO_SetBits(GPIOE, v);
+    	GPIOE->ODR = v << 8;
+    	v = (v << 1) % 0x00FF;
+
+    	//GPIO_Set
+    	//GPIOE->ODR = 512;
     	myDelay(3200000);
-    	GPIO_ResetBits(GPIOE, GPIO_Pin_9);
-    	myDelay(3200000);
+    	//GPIO_ResetBits(GPIOE, 0xFF00);
+    	//GPIOE->ODR = 0;
+    	//myDelay(3200000);
     }
 
     return 0;
