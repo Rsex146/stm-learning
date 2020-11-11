4ac1ef556b1261769d3142c3bec6d029d237eee8
Kostya Ryabinin <kostya@nulana.com>
2020-11-11 19:34:39 +0500

    kinda sinus

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 
 #include "stm32f30x_gpio.h"
 #include "stm32f30x_rcc.h"
+#include <math.h>
 
 
 void myDelay(uint32_t t)
@@ -10,11 +11,11 @@ void myDelay(uint32_t t)
 	for (i = 0; i < t; ++i){__NOP();};
 }
 
-void PWM(uint32_t tau, uint32_t T)
+void PWM(uint32_t tau, uint32_t T, uint16_t pin)
 {
-	GPIO_SetBits(GPIOE, GPIO_Pin_9);
+	GPIO_SetBits(GPIOE, pin);
 	myDelay(tau);
-	GPIO_ResetBits(GPIOE, GPIO_Pin_9);
+	GPIO_ResetBits(GPIOE, pin);
 	myDelay(T - tau);
 }
 
@@ -23,16 +24,22 @@ int main()
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
     GPIO_InitTypeDef g;
 
-    g.GPIO_Pin = GPIO_Pin_9;
+    g.GPIO_Pin = 0xFF00;
     g.GPIO_Mode = GPIO_Mode_OUT;
     g.GPIO_Speed = GPIO_Speed_Level_1;
     g.GPIO_OType = GPIO_OType_PP;
     g.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOE, &g);
 
+    float a = 0.0f;
+
     while (1)
     {
-    	PWM(5, 1000);
+    	for (uint8_t i = 0; i < 8; ++i)
+    		PWM((sinf(a + 2 * 3.14f * i / 8) + 1) * 500, 1000, 0x0100 << i);
+    	a += 0.01f;
+    	if (a > 6.28f)
+    		a = 0.0f;
     }
 
     return 0;
