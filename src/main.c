5ccd47847382b4b6cdadb44303413de8a0018264
Kostya Ryabinin <kostya@nulana.com>
2020-11-18 21:01:37 +0500

    dynamically changing pwm pulse via timer interrupt

--- src/main.c
+++ src/main.c
@@ -27,17 +27,20 @@ void tim()
 	TIM_TimeBaseInitTypeDef t;
 	TIM_TimeBaseStructInit(&t);
 	t.TIM_CounterMode = TIM_CounterMode_Up;
-	t.TIM_Prescaler = 36000;
+	t.TIM_Prescaler = 3600;
 	t.TIM_Period = 100;
 	TIM_TimeBaseInit(TIM1, &t);
 	TIM_Cmd(TIM1, ENABLE);
+	TIM_CtrlPWMOutputs(TIM1, ENABLE);
+
+	TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
 	TIM_CtrlPWMOutputs(TIM1, ENABLE);
 }
 
 void pwm()
 {
 	TIM_OCInitTypeDef p;
-	p.TIM_Pulse = 10;
+	p.TIM_Pulse = 0;
 	p.TIM_OCMode = TIM_OCMode_PWM1;
 	p.TIM_OutputState = TIM_OutputState_Enable;
 	p.TIM_OutputNState = TIM_OutputNState_Enable;
@@ -48,9 +51,31 @@ void pwm()
 	TIM_OC1Init(TIM1, &p);
 }
 
+void nvic()
+{
+	NVIC_InitTypeDef n;
+	n.NVIC_IRQChannel = TIM1_UP_TIM16_IRQn;
+	n.NVIC_IRQChannelPreemptionPriority = 0;
+	n.NVIC_IRQChannelSubPriority = 0;
+	n.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&n);
+}
+
+void TIM1_UP_TIM16_IRQHandler()
+{
+	static uint16_t pulse = 0;
+	static uint8_t ph = 1;
+	TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+	pulse += ph ? 1 : -1;
+	if (pulse == 100 || pulse == 0)
+		ph = !ph;
+	TIM1->CCR1 = pulse;
+}
+
 int main()
 {
 	gpio();
+	nvic();
 	tim();
 	pwm();
 
