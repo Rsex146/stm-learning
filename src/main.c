1ecbefd9ba0058bc5aa569bf8a39ab97b6b0d321
Kostya Ryabinin <kostya@nulana.com>
2020-11-25 20:39:41 +0500

    dma works in m2m mode

--- src/main.c
+++ src/main.c
@@ -1,17 +1,13 @@
 
 #include "stm32f30x_gpio.h"
 #include "stm32f30x_rcc.h"
-#include "stm32f30x_adc.h"
-#include "stm32f30x_tim.h"
-#include "stm32f30x_exti.h"
-#include "stm32f30x_syscfg.h"
+#include "stm32f30x_dma.h"
 
 
-#define LED(i) (1 << (8 + ((i) % 8)))
-#define MIN(a, b) ((a) < (b) ? a : b)
+#define DMA_BUFF_SIZE 4
 
-static volatile uint32_t g_adcVal = 0;
-static volatile uint8_t g_updateMax = 0;
+volatile uint16_t dst[DMA_BUFF_SIZE] = {0};
+volatile uint16_t src[DMA_BUFF_SIZE] = { 0x0100, 0x0200, 0x0400, 0x0800 };
 
 
 void myDelay(uint32_t t)
@@ -21,27 +17,11 @@ void myDelay(uint32_t t)
 	for (i = 0; i < t; ++i) { __NOP(); };
 }
 
-void myPWM(int32_t tau, int32_t T, uint16_t pinAverse, uint16_t pinReverse)
+void DMA1_Channel1_IRQHandler(void)
 {
-	if (tau >= T)
-	{
-		GPIO_SetBits(GPIOE, pinAverse);
-		GPIO_ResetBits(GPIOE, pinReverse);
-	}
-	else if (tau > 0)
-	{
-		GPIO_SetBits(GPIOE, pinAverse);
-		GPIO_ResetBits(GPIOE, pinReverse);
-		myDelay((uint32_t)tau);
-		GPIO_ResetBits(GPIOE, pinAverse);
-		GPIO_SetBits(GPIOE, pinReverse);
-		myDelay((uint32_t)(T - tau));
-	}
-	else
-	{
-		GPIO_ResetBits(GPIOE, pinAverse);
-		GPIO_SetBits(GPIOE, pinReverse);
-	}
+	GPIOE->ODR = dst[0] | dst[1] | dst[2] | dst[3];
+	DMA_ClearITPendingBit(DMA1_IT_TC1);
+	DMA_Cmd(DMA1_Channel1, DISABLE);
 }
 
 void gpio()
@@ -71,103 +51,51 @@ void gpio()
 	GPIO_Init(GPIOA, &g);
 }
 
-void adc()
+void dma()
 {
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE);
-	RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div10);
-
-	ADC_InitTypeDef a;
-	ADC_StructInit(&a);
-
-	a.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Enable;
-	a.ADC_Resolution = ADC_Resolution_12b;
-	a.ADC_ExternalTrigConvEvent = ADC_ExternalTrigConvEvent_0;
-	a.ADC_ExternalTrigEventEdge = ADC_ExternalTrigEventEdge_None;
-	a.ADC_DataAlign = ADC_DataAlign_Right;
-	a.ADC_OverrunMode = DISABLE;
-	a.ADC_AutoInjMode = DISABLE;
-	a.ADC_NbrOfRegChannel = 1;
-	ADC_Init(ADC1, &a);
-
-	ADC_Cmd(ADC1, ENABLE);
-
-	ADC_ITConfig(ADC1, ADC_IT_EOC, ENABLE);
-
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 1, ADC_SampleTime_7Cycles5);
-	while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_RDY));
-	ADC_StartConversion(ADC1);
-}
-
-void nvic()
-{
-	NVIC_InitTypeDef n;
-	n.NVIC_IRQChannel = ADC1_2_IRQn;
-	n.NVIC_IRQChannelPreemptionPriority = 0;
-	n.NVIC_IRQChannelSubPriority = 0;
-	n.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&n);
-
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
-
-	EXTI_InitTypeDef e;
-	e.EXTI_Line = EXTI_Line0;
-	e.EXTI_Mode = EXTI_Mode_Interrupt;
-	e.EXTI_Trigger = EXTI_Trigger_Rising;
-	e.EXTI_LineCmd = ENABLE;
-	EXTI_Init(&e);
-
-	NVIC_InitTypeDef nv;
-	nv.NVIC_IRQChannel = EXTI0_IRQn;
-	nv.NVIC_IRQChannelPreemptionPriority = 0;
-	nv.NVIC_IRQChannelSubPriority = 0;
-	nv.NVIC_IRQChannelCmd = ENABLE;
-
-	NVIC_Init(&nv);
-}
-
-void ADC1_2_IRQHandler(void)
-{
-	g_adcVal = ADC_GetConversionValue(ADC1);
-	ADC_ClearITPendingBit(ADC1, ADC_IT_EOC);
-}
-
-void EXTI0_IRQHandler()
-{
-	g_updateMax = 1;
-	EXTI_ClearFlag(EXTI_Line0);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+	DMA_InitTypeDef d;
+	DMA_StructInit(&d);
+	d.DMA_PeripheralBaseAddr = (uint32_t)&src;
+	d.DMA_MemoryBaseAddr = (uint32_t)&dst;
+	d.DMA_DIR = DMA_DIR_PeripheralSRC;
+	d.DMA_BufferSize = 10;
+	d.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
+	d.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	d.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	d.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	d.DMA_Mode = DMA_Mode_Normal;
+	d.DMA_Priority = DMA_Priority_Medium;
+	d.DMA_M2M = DMA_M2M_Enable;
+	DMA_Init(DMA1_Channel1, &d);
+	DMA_Cmd(DMA1_Channel1, DISABLE);
+	DMA_ITConfig(DMA1_Channel1, DMA_IT_TC, ENABLE);
+	NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 }
 
 int main()
 {
 	gpio();
-	nvic();
-	adc();
+	dma();
 
-	uint16_t pinFrom;
-	uint16_t pinTo;
-	int32_t t;
-	uint32_t ledIdx;
-	uint32_t val;
-	uint32_t maxVal = 4096;
+	uint8_t f = 0;
 
 	while (1)
 	{
-		if (g_updateMax)
+		if (((GPIOA->IDR & (1 << 0)) == 1) && (f == 0))
 		{
-			maxVal = g_adcVal;
-			g_updateMax = 0;
+			myDelay(50000);
+
+			if (((GPIOA->IDR & (1 << 0)) == 1) && (f == 0))
+			{
+				DMA_Cmd(DMA1_Channel1, ENABLE);
+				f = 1;
+			}
 		}
-		val = g_adcVal;
-		if (val > maxVal)
-			val = 4096;
-		ledIdx = val / 512;
-		pinFrom = LED(ledIdx);
-		pinTo = LED(ledIdx + 1);
-		t = val % 512;
-		GPIOE->ODR &= (pinFrom | pinTo);
-		myPWM(t, 512, pinTo, pinFrom);
+		if (((GPIOA->IDR & (1 << 0)) == 0) && (f == 0))
+		{
+			f = 0;
+		}
 	};
 
     return 0;
