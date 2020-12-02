d2470e98505261486151e112ef33b1fce7845298
Kostya Ryabinin <kostya@nulana.com>
2020-12-02 20:04:33 +0500

    button-debounce

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
 
 #include "stm32f30x_gpio.h"
 #include "stm32f30x_rcc.h"
-#include <math.h>
-
-#define MAX_LIGHTNESS 1000
-#define DROP_INTERVAL 200
 
 
 void myDelay(uint32_t t)
@@ -14,32 +10,13 @@ void myDelay(uint32_t t)
 	for (i = 0; i < t; ++i){__NOP();};
 }
 
-void myPWM(int32_t tau, int32_t T, uint16_t pin)
-{
-	if (tau >= T)
-	{
-		GPIO_SetBits(GPIOE, pin);
-	}
-	else if (tau > 0)
-	{
-		GPIO_SetBits(GPIOE, pin);
-		myDelay((uint32_t)tau);
-		GPIO_ResetBits(GPIOE, pin);
-		myDelay((uint32_t)(T - tau));
-	}
-	else
-	{
-		GPIO_ResetBits(GPIOE, pin);
-	}
-}
-
 int main()
 {
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
     GPIO_InitTypeDef g;
 
-    g.GPIO_Pin = 0xFF00;
+    g.GPIO_Pin = GPIO_Pin_8;
     g.GPIO_Mode = GPIO_Mode_OUT;
     g.GPIO_Speed = GPIO_Speed_Level_1;
     g.GPIO_OType = GPIO_OType_PP;
@@ -52,32 +29,27 @@ int main()
     g.GPIO_PuPd = GPIO_PuPd_DOWN;
     GPIO_Init(GPIOA, &g);
 
-    float lightness = 0.0f;
-    int drop = 0;
+    uint8_t pushed = 0;
 
     while (1)
     {
     	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 1)
     	{
-    		++drop;
-    		lightness += 1.0f;
-    		if ((uint32_t)lightness > MAX_LIGHTNESS * 8)
+    		if (!pushed)
     		{
-    			lightness = (float)(MAX_LIGHTNESS * 8);
-    			myDelay(50000);
+				myDelay(50000);
+				if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 1)
+				{
+					pushed = 1;
+					GPIOE->ODR ^= GPIO_Pin_8;
+				}
     		}
     	}
     	else
     	{
-			if (drop > 0 && drop < DROP_INTERVAL)
-				lightness = 0.0f;
-			drop = 0;
+    		pushed = 0;
     	}
-    	for (uint8_t i = 0; i < 8; ++i)
-    		myPWM(((uint32_t)lightness - MAX_LIGHTNESS * i), MAX_LIGHTNESS, 1 << (i + 8));
     }
 
     return 0;
 }
-
-
