c828cd341ab32402c591e73cf5cbb1e03e84b306
Kostya Ryabinin <kostya@nulana.com>
2020-12-04 02:31:32 +0500

    interrupt->dma->uart1->uart2->led

--- src/main.c
+++ src/main.c
@@ -4,7 +4,14 @@
 #include "stm32f30x_misc.h"
 #include "stm32f30x_tim.h"
 #include "stm32f30x_spi.h"
+#include "stm32f30x_dma.h"
 
+
+#define DMA_BUFF_SIZE 4
+
+volatile uint8_t g_src[DMA_BUFF_SIZE] = { 1, 2, 3, 4 };
+volatile uint8_t g_dst[DMA_BUFF_SIZE] = { 0 };
+volatile uint8_t g_dataReady = 0;
 
 void myDelay(uint32_t t)
 {
@@ -83,6 +90,28 @@ void led()
 	GPIO_Init(GPIOE, &g);
 }
 
+void buttonInterrupt()
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+
+	EXTI_InitTypeDef e;
+	e.EXTI_Line = EXTI_Line0;
+	e.EXTI_Mode = EXTI_Mode_Interrupt;
+	e.EXTI_Trigger = EXTI_Trigger_Rising;
+	e.EXTI_LineCmd = ENABLE;
+	EXTI_Init(&e);
+
+	NVIC_InitTypeDef nv;
+	nv.NVIC_IRQChannel = EXTI0_IRQn;
+	nv.NVIC_IRQChannelPreemptionPriority = 0;
+	nv.NVIC_IRQChannelSubPriority = 0;
+	nv.NVIC_IRQChannelCmd = ENABLE;
+
+	NVIC_Init(&nv);
+}
+
 void button()
 {
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
@@ -96,6 +125,37 @@ void button()
 	g.GPIO_OType = GPIO_OType_PP;
 	g.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_Init(GPIOA, &g);
+
+	buttonInterrupt();
+}
+
+void EXTI0_IRQHandler()
+{
+	DMA_Cmd(DMA1_Channel1, ENABLE);
+
+	EXTI_ClearFlag(EXTI_Line0);
+}
+
+void dma()
+{
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+	DMA_InitTypeDef d;
+	DMA_StructInit(&d);
+	d.DMA_PeripheralBaseAddr = (uint32_t)&g_src;
+	d.DMA_MemoryBaseAddr = (uint32_t)&g_dst;
+	d.DMA_DIR = DMA_DIR_PeripheralSRC;
+	d.DMA_BufferSize = 4;
+	d.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
+	d.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	d.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+	d.DMA_MemoryDataSize = DMA_PeripheralDataSize_Byte;
+	d.DMA_Mode = DMA_Mode_Normal;
+	d.DMA_Priority = DMA_Priority_Medium;
+	d.DMA_M2M = DMA_M2M_Enable;
+	DMA_Init(DMA1_Channel1, &d);
+	DMA_Cmd(DMA1_Channel1, DISABLE);
+	DMA_ITConfig(DMA1_Channel1, DMA_IT_TC, ENABLE);
+	NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 }
 
 void usart1_send(uint8_t ch)
@@ -104,44 +164,38 @@ void usart1_send(uint8_t ch)
 	USART_SendData(USART1, ch);
 }
 
+void DMA1_Channel1_IRQHandler(void)
+{
+	g_dataReady = 1;
+
+	DMA_ClearITPendingBit(DMA1_IT_TC1);
+	DMA_Cmd(DMA1_Channel1, DISABLE);
+}
+
 void USART2_IRQHandler()
 {
 	if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
 	{
-		GPIOE->ODR = 1 << (USART_ReceiveData(USART2) + 8);
+		GPIOE->ODR |= 1 << (USART_ReceiveData(USART2) + 8);
 	}
 }
-
-void button_scan()
-{
-	static uint8_t f = 0;
-	static uint8_t k = 0;
-	if ((GPIOA->IDR & 0x1) && (f == 0))
-	{
-		myDelay(50000);
-		if ((GPIOA->IDR & 0x1) && (f == 0))
-		{
-			usart1_send((k++) % 8);
-			f = 1;
-		}
-	}
-	if (!(GPIOA->IDR & 0x1) && (f == 1))
-	{
-		f = 0;
-	}
-}
-
 
 int main()
 {
 	led();
 	button();
+	dma();
 	usart1();
 	usart2();
 
 	while (1)
 	{
-		button_scan();
+		if (g_dataReady)
+		{
+			g_dataReady = 0;
+			for (uint8_t i = 0; i < DMA_BUFF_SIZE; ++i)
+				usart1_send(g_dst[i]);
+		}
 	};
 
     return 0;
