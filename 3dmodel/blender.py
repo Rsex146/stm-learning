0e76c6cc531b6eb310a786ae0dfdc78c41eb72e5
Kostya Ryabinin <kostya@nulana.com>
2020-12-09 17:56:54 +0500

    fix blender plugin and add sintel model

---
+++ 3dmodel/blender.py
@@ -0,0 +1,32 @@
+
+import bpy
+import serial
+import json
+
+
+def poll():
+    # obj1 = bpy.data.objects["Cube"]
+    # obj1.rotation_mode = "QUATERNION"
+    # obj2 = bpy.data.objects["Cube.001"]
+    # obj2.rotation_mode = "QUATERNION"
+    obj1 = bpy.data.objects["Armature"].pose.bones["arm right shoulder 2"]
+    obj2 = bpy.data.objects["Armature"].pose.bones["arm right elbow"]
+    sPort = serial.Serial('/dev/tty.usbmodem205F366850421')
+    sPort.readline()
+    data = sPort.readline()
+    try:
+        jData = json.loads(data.decode("utf-8").strip().strip("\x00"))
+    except:
+        jData = { "q1": [0, 0, 0, 1], "q2": [0, 0, 0, 1] }
+    obj1.rotation_quaternion[2] = jData["q1"][0];
+    obj1.rotation_quaternion[1] = -jData["q1"][1];
+    obj1.rotation_quaternion[3] = jData["q1"][2];
+    obj1.rotation_quaternion[0] = jData["q1"][3];
+    obj2.rotation_quaternion[2] = jData["q2"][0];
+    obj2.rotation_quaternion[1] = -jData["q2"][1];
+    obj2.rotation_quaternion[3] = jData["q2"][2];
+    obj2.rotation_quaternion[0] = jData["q2"][3];
+    return 0.016
+
+bpy.app.timers.register(poll)
+
