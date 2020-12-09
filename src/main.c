7a85ae96467eb3c63d391b1d82a98b9a9b65779e
Kostya Ryabinin <kostya@nulana.com>
2020-12-09 21:02:57 +0500

    ooook, no time to tell eclipse were are includes, sorry

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 
+#include "FreeRTOSConfig.h"
+#include "projdefs.h"
+#include "portmacro.h"
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "croutine.h"
+#include "task.h"
+#include "queue.h"
+
 #include "stm32f30x_gpio.h"
 #include "stm32f30x_rcc.h"
-#include <math.h>
 
-#define MAX_LIGHTNESS 1000
+/*#define MAX_LIGHTNESS 1000
 #define DROP_INTERVAL 200
 
 
@@ -79,5 +87,64 @@ int main()
 
     return 0;
 }
+*/
 
+void vApplicationIdleHook ( void ){}
+void vApplicationMallocFailedHook ( void ){for ( ;; );}
+void vApplicationStackOverflowHook ( xTaskHandle pxTask, char *pcTaskName ){
+( void ) pcTaskName;
+( void ) pxTask;
+for ( ;; );}
+void vApplicationTickHook ( void ){}
+
+void gpio()
+{
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
+	GPIO_InitTypeDef g;
+
+	g.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
+	g.GPIO_Mode = GPIO_Mode_OUT;
+	g.GPIO_Speed = GPIO_Speed_Level_1;
+	g.GPIO_OType = GPIO_OType_PP;
+	g.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOE, &g);
+}
+
+void taskLED1()
+{
+	while (1)
+	{
+		GPIO_SetBits(GPIOE, GPIO_Pin_13);
+		vTaskDelay(500);
+		GPIO_ResetBits(GPIOE, GPIO_Pin_13);
+		vTaskDelay(500);
+	}
+}
+
+void taskLED2()
+{
+	while (1)
+	{
+		GPIO_SetBits(GPIOE, GPIO_Pin_14);
+		vTaskDelay(400);
+		GPIO_ResetBits(GPIOE, GPIO_Pin_14);
+		vTaskDelay(400);
+	}
+}
+
+int main()
+{
+	gpio();
+
+	xTaskCreate(taskLED1, (char *)"LED1", configMINIMAL_STACK_SIZE, NULL, 2, (xTaskHandle *)NULL);
+	xTaskCreate(taskLED2, (char *)"LED2", configMINIMAL_STACK_SIZE, NULL, 2, (xTaskHandle *)NULL);
+	vTaskStartScheduler();
+
+	while (1)
+	{
+		__NOP();
+	}
+
+	return 0;
+}
 
